f8763d356b6d851bd36dff6711f9fc5a2cd3b24c
kd1439058-hash <kd1439058@st.kobedenshi.ac.jp>
2026-06-24 11:52:42 +0900

    WinInfoクラスの追加とプロジェクト構成の修正
    WinInfo.csを追加し、ウィンドウ情報を管理するクラスを実装しました。また、NativeMethods.csをWin32API.csに書き換えて、整理しました。

---
+++ Models/WinInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Window_Priority_Layout.Models
+{
+    // WinInfoクラスは、ウィンドウの情報を格納するためのクラスです。
+    internal class WinInfo
+    {
+        // ウィンドウの番号（ハンドル）を格納するプロパティ
+        public IntPtr Handle { get; set; }
+
+        // ウィンドウのタイトルを格納するプロパティ
+        //例：”(ユーザー名)-Google Chrome”
+        public string Title { get; set; }
+
+        // ウィンドウのプロセス名を格納するプロパティ
+        //例：”Google Chrome”
+        public string ProcessName { get; set; }
+
+        // ウィンドウの優先度を格納するプロパティ
+        public int Priority { get; set; }
+
+
+
+    }
+}
